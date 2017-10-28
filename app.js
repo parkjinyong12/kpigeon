808716086725345ffb3fc075564a98e2ddee62ad
parkjinyong12 <bh9578@gmail.com>
2017-10-28 15:42:22 +0900

    연동 테스트

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var router = express.Router();
     
 /*Express 서버 시작*/
 http.createServer(app).listen(app.get('port'), function() {
-    console.log('익스프레스 서버를 시작했습니다 : ' + app.get('port'));
+    console.log('익스프레스 서버를 시작했습니다(개발) : ' + app.get('port'));
 });
 
 
