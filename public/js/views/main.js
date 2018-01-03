b2478b2e50726b2ad94d04caba95a25b7a901671
parkjinyong12 <bh9578@gmail.com>
2018-01-03 22:53:01 +0900

    수정

--- public/js/views/main.js
+++ public/js/views/main.js
@@ -1,38 +1,65 @@
 $(document).ready(function() {
 
+    // Timeout 이벤트
+    var e_clearTiemout;    
+
     // city
-    $(".city-spot").hover(function() {
-        $(this).attr('src','/img/heart_spot_small_on_2.png');
-    },function() {
-        $(this).attr('src','/img/heart_spot_small.png');
-    });
+    $(".city-spot").hover(function() {    
+        setTimeout(function() {
+            $(".city-spot").attr('src','/img/heart_spot_small_on_2.png');
+            refresh_board();
+        }, 300);    
+        e_clearTiemout = setTimeout(function() {
+            $(".city-spot").attr('src','/img/heart_spot_small.png');
+        }, 1000);                            
+                
+    },function() {       
+        clearTiemout(e_clearTiemout);
+        setTimeout(function() {
+            $(".city-spot").attr('src','/img/heart_spot_small.png');
+        }, 300);  
+    });   
 
     // lang
     $(".lang").click(function() {
         $(".lang-body").html($(this).find("span").html());
     });
+    
 
-    $(".travel").hover(function() {
-        $('body').css('backgroundColor','yellow');
-    },function() {
-        $('body').css('backgroundColor','white');
-    });
+    //fnCtlPicCount(3);
 
-    $(".seesight").hover(function() {
-        $('body').css('backgroundColor','green');
-    },function() {
-        $('body').css('backgroundColor','white');
-    });
-
-    $(".culture").hover(function() {
-        $('body').css('backgroundColor','blue');
-    },function() {
-        $('body').css('backgroundColor','white');
-    });
-
-    $(".accommodation").hover(function() {
-        $('body').css('backgroundColor','red');
-    },function() {
-        $('body').css('backgroundColor','white');
+    $( window ).resize(function() {
+       //fnCtlPicCount(3);
     });
 });
+
+//
+function fnCtlPicCount(num) {
+
+    var m = window.matchMedia("screen and (max-width: 768px)");
+    
+    if(m.matches) {                
+        var num = 2;
+    }
+
+    // 한줄에 들어가는 사진 수 조정.
+    var row_placeholder = $(".city-main-board .row.placeholder");          
+    
+    console.log(row_placeholder.length);
+     for(var i = 0; i < row_placeholder.length; i++) {
+         if(i%num == 0) {            
+             console.log(i);    
+         } else {
+             $(row_placeholder[i]).hide();
+         }
+    }
+}
+
+// city board
+function refresh_board() {
+    $(".city-main-board h1").html("Busan");
+    
+}
+
+// .row.placeholder
+
--- routes/screen.js
+++ routes/screen.js
@@ -8,6 +8,9 @@ Screen.route = function() {
   router.get('/main',function(req,res) {
     res.render('main/main', { title: 'Express' })
   });
+  router.get('/base',function(req,res) {
+    res.render('main/base', { title: 'Express' })
+  });
   router.get('/sub',function(req,res) {
     res.sendFile(path.join(__dirname,'/views/sub/sub.html'))
   });
