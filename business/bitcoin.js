152e8f63fc37a9eb16a50310f03a6e035f4963db
박진용 <bh9578@gmail.com>
2018-02-25 15:58:13 +0900

    동기화 방식 변경

--- business/bitcoin.js
+++ business/bitcoin.js
@@ -11,22 +11,28 @@ Bitcoin.route = function() {
   router.get('/recordHistory',function(req,res) {    
    
     coin_type=req.query.coin;
-    var pool = mysql.getPool();  
-    url = "https://api.bithumb.com/public/ticker/" + coin_type;
-   
-    var closing_price;
+    var pool = mysql.getPool();
+
+    var orin_data = [];
+    orin_data.url = "https://api.bithumb.com/public/ticker/";
+    orin_data.coin_type = coin_type;
+
     var tasks = [        
-        function(callback) {
+        function(orin_data,callback) {            
+            url=orin_data.url+orin_data.coin_type;
             request.get(url, function(err, res, result) {
                 console.log("closing_price : " + JSON.parse(result).data.closing_price);   
                 closing_price = JSON.parse(result).data.closing_price;
-                coin_type=coin_type;
-                callback(null);
+               
+                next_data=[];
+                next_data.type=orin_data.coin_type; 
+                next_data.price=closing_price;     
+                callback(null,next_data);
             });
         },
-        function(callback) {
+        function(next_data,callback) {
             pool.getConnection(function(err,connection){                 
-                var query = connection.query('insert into price_history (coin_type, price) values (?, ?)',[coin_type,closing_price],function (err, result) {
+                var query = connection.query('insert into price_history (coin_type, price) values (?, ?)',[next_data.type,next_data.price],function (err, result) {
                     if (err) {
                         console.error(err);    
                         throw err;  
@@ -36,7 +42,7 @@ Bitcoin.route = function() {
             });        
         }
     ];
-    async.series(tasks, function(err, results) {
+    async.waterfall(tasks, function(err, results) {
         console.log(results);        
     });
     res.send(200,'success');    
