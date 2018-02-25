608ec30018d47a8fef7173b94bdf4777e45dd9f4
박진용 <bh9578@gmail.com>
2018-02-25 15:46:28 +0900

    수정

--- business/bitcoin.js
+++ business/bitcoin.js
@@ -39,42 +39,7 @@ Bitcoin.route = function() {
         console.log(results);        
     });
     res.send(200,'success');    
-  });
-
-  router.get('/recordHistory',function(req,res) {    
-   
-    var pool = mysql.getPool();  
-    url = "https://api.bithumb.com/public/ticker/ALL"
-   
-    var closing_price;
-    var tasks = [        
-        function(callback) {
-            request.get(url, function(err, res, result) {
-                console.log("closing_price : " + JSON.parse(result).data.ETH.closing_price);   
-                closing_price = JSON.parse(result).data.ETH.closing_price;
-                callback(null);
-            });
-        },
-        function(callback) {
-            pool.getConnection(function(err,connection){                 
-                var query = connection.query('insert into price_history (coin_type, price) values (?, ?)',['ETH',closing_price],function (err, result) {
-                    if (err) {
-                        console.error(err);    
-                        throw err;  
-                    }    
-                    console.log(query);            
-                });
-            });        
-        }
-    ];   
-
-    async.series(tasks, function(err, results) {
-        console.log(results);        
-    });
-
-    res.send(200,'success');
-    
-  });
+  });  
   return router;
 }
 
